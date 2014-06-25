ab7e90e2f33a9ad680d21f3f75e562732da14084
Kyle Kirkby <kylekirkby@googlemail.com>
2014-06-25 10:29:24 +0100

    New

--- cropProgram.py
+++ cropProgram.py
@@ -11,16 +11,23 @@ class CropWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Crop Simulator")
+        self.resize(300,150)
         self.create_select_crop_layout()
 
     def create_select_crop_layout(self):
         #this is the initial layout of the window - select crop type
 
         self.crop_radio_buttons = RadioButtonWidget("Crop Simulation","Please select a crop",("Wheat","Potato"))
-        self.setCentralWidget(self.crop_radio_buttons)
         self.instantiate_button = QPushButton("Create Crop")
+        
+        self.initial_layout = QVBoxLayout()
+        self.initial_layout.addWidget(self.crop_radio_buttons)
+        self.initial_layout.addWidget(self.instantiate_button)
 
-        #create layout to hold the widgets
+        self.select_crop_widget = QWidget()
+        self.select_crop_widget.setLayout(self.initial_layout)
+
+        self.setCentralWidget(self.select_crop_widget)
 
 def main():
     crop_simulation = QApplication(sys.argv)
